6a365e33df88bed162a4c4a640c6f72e5cb5215d
Richard Pope <richard@memespring.co.uk>
2014-11-19 14:48:09 +0000

    removed qrcodes as library is shakey

--- fishing/__init__.py
+++ fishing/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_oauthlib.client import OAuth
-from flask.ext.qrcode import QRcode
 
 #app
 app = Flask(__name__)
 app.config.from_object('config')
 oauth = OAuth(app)
-QRcode(app)
 
 from fishing import views
