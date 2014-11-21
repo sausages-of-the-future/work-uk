977f03023785ba88f0e15cb624ed8f29d5be35ec
Richard Pope <richard@memespring.co.uk>
2014-11-21 13:40:20 +0000

    working

--- run_dev.py
+++ run_dev.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 from fishing import app
 import os
-app.run(host="0.0.0.0", port=int(os.environ['PORT'], 5000), debug=True)
+app.run(host="0.0.0.0", port=int(os.environ['PORT']), debug=True)
