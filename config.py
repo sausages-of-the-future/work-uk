1e9b1e221eabc7336bedda865e6b18f3fd028732
Richard Pope <richard@memespring.co.uk>
2014-11-21 13:01:12 +0000

    just about working with dev environment

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
+class Config(object):
+    DEBUG = False
 
-try:
-	from local_config import *
-except ImportError:
-	print "Local settings file not found. Try creating one. There is an example in local_config.py.git"
-	exit()
+    SECRET_KEY = os.environ['SECRET_KEY']
+    BASE_URL = 'http://localhost:5001'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestConfig(DevelopmentConfig):
+    TESTING = True
