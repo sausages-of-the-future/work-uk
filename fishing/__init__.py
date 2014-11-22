7a5b35bb88a8673a98fe8bb5a0c0bf6a41e2120d
Richard Pope <richard@memespring.co.uk>
2014-11-22 11:38:03 +0000

    config

--- fishing/__init__.py
+++ fishing/__init__.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_oauthlib.client import OAuth
 
 #app
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(os.environ.get('SETTINGS'))
 oauth = OAuth(app)
 
 from fishing import views
