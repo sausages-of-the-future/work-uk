91bcc853ced4017caf6ec8fb0dcdc3be8efe5707
Adam Shimali <adam.shimali@gmail.com>
2015-01-27 00:17:20 +0000

    add sentry

--- work_uk/__init__.py
+++ work_uk/__init__.py
@@ -12,4 +12,8 @@ import urllib
 url = urllib.parse.urlparse(app.config.get('REDISCLOUD_URL', 'redis://user:@localhost:6379'))
 redis_client = Redis(host=url.hostname, port=url.port, password=url.password)
 
+if 'SENTRY_DSN' in os.environ:
+    from raven.contrib.flask import Sentry
+    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
+
 from work_uk import views
